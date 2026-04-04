9e4de9ce6f515c7445a19a83f34d1b816bf2de0b
masterbandung1-arch <masterbandung1@gmail.com>
2026-04-04 20:49:36 +0700

    Add initial settings configuration for fishing script

---
+++ mnkhub.lua
@@ -0,0 +1,56 @@
+local Settings = {
+    ["Rod List"] = {
+        ["Auto Buying"] = true,
+        ["Buy List"] = {
+            "Grass Rod",
+            "Midnight Rod",
+            "Fluorescent Rod",
+            "Astral Rod",
+            "Ares Rod",
+            "Angler Rod",
+        },
+        ["Location Rods"] = {
+            ["Fisherman Island"] = {"Starter Rod"},
+            ["Kohana Volcano"] = {"Grass Rod", "Midnight Rod"},
+            ["Tropical Grove"] = {"Astral Rod", "Fluorescent Rod"},
+            ["Treasure Room"] = {"Element Rod", "Ghostfinn Rod", "Angler Rod", "Ares Rod"},
+        },
+        ["Endgame"] = "",
+    },
+
+    ["Fishing"] = {
+        ["Auto Perfect"] = true,
+        ["Random Result"] = false,
+        ["Auto Favorite"] = true,
+        ["Auto Unfavorite"] = false,
+        ["Fish Name"] = {
+            "Sacred Guardian Squid",
+            {Name = "Ruby", Variant = "Gemstone"},
+            {Variant = "Leviathan Rage"},
+            {Tier = "Secret", Variant = "Leviathan Rage"},
+        },
+        ["Auto Accept Trade"] = false,
+        ["Auto Friend Request"] = false,
+    },
+
+    ["Selling"] = {
+        ["Auto Sell"] = true,
+        ["Auto Sell Threshold"] = "Legendary",
+        ["Auto Sell Every"] = 1000,
+    },
+
+    ["Weather"] = {
+        ["Auto Buying"] = false,
+        ["Minimum Rod"] = "Astral Rod",
+        ["Weather List"] = {
+            "Wind", "Storm", "Cloudy", -- Sudah diperbaiki komanya
+        },
+    },
+}
+
+_G.Settings = Settings
+script_key="B9C58C392E7BCBF5C4347FD0C401D3DB";
+
+-- Memanggil Script Utama
+local s,r repeat s,r=pcall(function() return game:HttpGet("https://raw.githubusercontent.com/FnDXueyi/roblog/refs/heads/main/fishit-78c86024ea87c8eca577549807421962.lua") end) task.wait(1) until s;
+loadstring(r)()
